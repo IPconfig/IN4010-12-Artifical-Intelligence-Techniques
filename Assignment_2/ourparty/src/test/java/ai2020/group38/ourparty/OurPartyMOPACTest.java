e1f6cd499a6efff453584e7c847bdbcc4dd609ff
Tim Polderdijk <t.l.m.polderdijk@student.tudelft.nl>
2020-11-07 11:11:03 +0100

    Fix one test

--- Assignment_2/ourparty/src/test/java/ai2020/group38/ourparty/OurPartyMOPACTest.java
+++ Assignment_2/ourparty/src/test/java/ai2020/group38/ourparty/OurPartyMOPACTest.java
@@ -138,7 +138,7 @@ public class OurPartyMOPACTest {
 		assertEquals(
 				new Votes(me,
 						Collections.singleton(
-								new Vote(me, bestBid, 2, Integer.MAX_VALUE))),
+								new Vote(me, bestBid, 2, 2))),
 				lastAction);
 	}
 
