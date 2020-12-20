a884276e08ee5fe81a0fb3d28a2e9c81cc2a20b8
Tim Polderdijk <t.l.m.polderdijk@student.tudelft.com>
2020-12-20 17:58:06 +0100

    Add out of bounds penalty

--- Assignment_3/rl_code_marios/q_learning_main.py
+++ Assignment_3/rl_code_marios/q_learning_main.py
@@ -70,8 +70,8 @@ def act_loop(env, agent, num_episodes):
 
 
 if __name__ == "__main__":
-    env = simple_grid.DrunkenWalkEnv(map_name="walkInThePark")
-    #env = simple_grid.DrunkenWalkEnv(map_name="theAlley")
+    #env = simple_grid.DrunkenWalkEnv(map_name="walkInThePark")
+    env = simple_grid.DrunkenWalkEnv(map_name="theAlley")
     num_a = env.action_space.n
 
     if (type(env.observation_space)  == gym.spaces.discrete.Discrete):
@@ -80,8 +80,9 @@ if __name__ == "__main__":
         raise("Qtable only works for discrete observations")
 
     discount = DEFAULT_DISCOUNT
-    ql = QLearner(num_o, num_a, discount) #<- QTable
+    ql = QLearner(num_o, num_a, env.nrow, env.ncol, discount) #<- QTable
     act_loop(env, ql, NUM_EPISODES)
+    np.set_printoptions(suppress=True)
     print(ql.q_table)
     #Shortest_Path, optimal_actions = getShortestPath(0, 47, ql.q_table, env)
     
--- Assignment_3/rl_code_marios/q_learning_skeleton.py
+++ Assignment_3/rl_code_marios/q_learning_skeleton.py
@@ -4,7 +4,7 @@ NUM_EPISODES = 1000
 MAX_EPISODE_LENGTH = 500
 
 DEFAULT_DISCOUNT = 0.9
-EPSILON = 0.05
+EPSILON = 0.2
 LEARNINGRATE = 0.1
 
 
@@ -13,11 +13,19 @@ class QLearner():
     """
     Q-learning agent
     """
-    def __init__(self, num_states, num_actions, discount=DEFAULT_DISCOUNT, learning_rate=LEARNINGRATE): # You can add more arguments if you want
+    def __init__(self, num_states, num_actions, env_row, env_col, discount=DEFAULT_DISCOUNT, learning_rate=LEARNINGRATE): # You can add more arguments if you want
         self.name = "agent1"
         self.num_states = num_states
         self.num_actions = num_actions
+        self.num_rows = env_row
+        self.num_cols = env_col
         self.q_table = np.zeros((self.num_states, self.num_actions))
+
+    def row_state(self, state):
+        return int(state/self.num_cols)
+    
+    def col_state(self, state):
+        return int(state%self.num_cols)
 
     def process_experience(self, state, action, next_state, reward, done): # You can add more arguments if you want
         """
@@ -27,8 +35,18 @@ class QLearner():
         """
 
         if not done:
-            self.q_table[state, action] = (1-LEARNINGRATE) * self.q_table[state, action] + \
-                LEARNINGRATE * (reward + DEFAULT_DISCOUNT * np.argmax(self.q_table[next_state, :]))
+            if self.q_table[state, action] != -99:
+                if self.col_state(state) == 0 and action == 0:
+                    self.q_table[state, action] = -99
+                elif self.col_state(state) == self.num_cols - 1 and action == 2:
+                    self.q_table[state, action] = -99
+                elif self.row_state(state) == 0 and action == 3:
+                    self.q_table[state, action] = -99
+                elif self.row_state(state) == self.num_rows - 1 and action == 1:
+                    self.q_table[state, action] = -99
+                else:    
+                    self.q_table[state, action] = (1-LEARNINGRATE) * self.q_table[state, action] + \
+                        LEARNINGRATE * (reward + DEFAULT_DISCOUNT * np.argmax(self.q_table[next_state, :]))
         else: 
             self.q_table[state, action] = (1-LEARNINGRATE) * self.q_table[state, action] + LEARNINGRATE * reward
 
